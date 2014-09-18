2d1c374d3c9af9593c59193f2db56362b6557b59
Hasan Karahan <hasan.karahan@blackhan.com>
2014-09-18 16:23:26 +0300

    DD-303: Pagers for last/next page

--- src/javascript/Main.js
+++ src/javascript/Main.js
@@ -10,7 +10,9 @@ Class("MarkdownReader.Main", {
                         MarkdownReader.Dizmo.setTitle(text);
                     }
 
-                    return marked.defaults.renderer.heading(text, level, raw);
+                    return marked.defaults.renderer.heading.call ({
+                        options: marked.defaults
+                    }, text, level, raw);
                 };
 
                 return {
@@ -122,6 +124,16 @@ Class("MarkdownReader.Main", {
                         jQuery('#front').empty().append(
                             '<div id="content">{0}</div>'.replace(
                                 '{0}', self.md2html.convert(value)));
+
+                        if (jQuery('#pager').length > 0) {
+                            jQuery('#pager-lhs').click(
+                                self.onPagerLhsClick.bind (self));
+                            jQuery('#pager-rhs').click(
+                                self.onPagerRhsClick.bind (self));
+                            self.showPage(function () {
+                                return 0;
+                            });
+                        }
                     }
                 });
                 self.setUrlMd(urlMd);
@@ -132,6 +144,63 @@ Class("MarkdownReader.Main", {
 
         onShowBack: function () {
             this.dizmo.my.setTitle('Markdown Reader');
+        },
+
+        onPagerLhsClick: function () {
+            this.showPage(function (page) {
+                return (page - 1 >= 0) ? page - 1 : 0;
+            });
+
+            return false;
+        },
+
+        onPagerRhsClick: function () {
+            this.showPage(function (page, pages) {
+                return (page + 1 < pages) ? page + 1 : page;
+            });
+
+            return false;
+        },
+
+        showPage: function (counter) {
+            var groups = this.group(
+                jQuery('#content > *').not ('#pager'), function (item) {
+                    return item.tagName.toLowerCase() == 'h2'.toLowerCase();
+                }
+            );
+
+            if (counter !== undefined) {
+                this.page = counter.call(this, this.page||0, groups.length);
+            } else {
+                this.page = 0;
+            }
+
+            for (var index=0; index<groups.length; index++) {
+                if (index == this.page) {
+                    jQuery(groups[index]).show();
+                } else {
+                    jQuery(groups[index]).hide();
+                }
+            }
+        },
+
+        group: function (array, by) {
+            var groups = [], index = null;
+            for (var i=0; i<array.length; i++) {
+                var item = array[i];
+                if (by (item, index, i)) {
+                    index = (index !== null) ? index + 1 : 0;
+                }
+                if (index !== null) {
+                    if (groups[index] === undefined) {
+                        groups[index] = [];
+                    }
+
+                    groups[index].push (item);
+                }
+            }
+
+            return groups;
         }
     }
 });
