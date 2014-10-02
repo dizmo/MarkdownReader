18fcb6d9e642a48f413876558083b2c2a8023f0f
Hasan Karahan <hasan.karahan@blackhan.com>
2014-10-02 18:06:56 +0300

    DD-303: MdReaderDizmoPlugin fix

--- grace-md-reader-dizmo/plugin.py
+++ grace-md-reader-dizmo/plugin.py
@@ -8,14 +8,13 @@ class MdReaderDizmoPlugin (DizmoPlugin):
     def after_build(self):
         DizmoPlugin.after_build (self)
 
-        name = self._config['name']
         properties = self._config.get ('dizmo_private', {})
         script = self._config['js_name'] + '.js'
         target = os.path.join(self._config['build_path'], script)
 
         if os.path.exists(target):
             with open (target, 'a') as file: file.writelines([
-                "\n%s.Dizmo.save ('%s', '%s');" % (name, k, v)
+                "\nMarkdownReader.Dizmo.save ('%s', '%s');" % (k, v)
                     for k, v in properties.items ()
             ])
 
