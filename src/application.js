c93dc8013813e7e53ce5c4c10d39338c601c54e8
Hasan Karahan <hasan.karahan@blackhan.com>
2015-07-20 09:11:04 +0400

    DD-857: rm'd hard coded CSS value

--- src/application.js
+++ src/application.js
@@ -31,10 +31,37 @@ jQuery(document).ready(function () {
         }
 
         if (MarkdownReader.Dizmo.load('tocFlag') === true) {
-            var w = MarkdownReader.Dizmo.getWidth(),
+            var css_rule = function (selector, property, parser) {
+                var sheets = document.styleSheets;
+                for (var s in sheets) {
+                    if (sheets.hasOwnProperty(s)) {
+                        var rules = sheets[s].rules;
+                        for (var r in rules) {
+                            if (rules.hasOwnProperty(r)) {
+                                var rule = rules[r];
+                                if (rule.selectorText === selector) {
+                                    if (property) {
+                                        var value = rule.style[property];
+                                        if (parser) {
+                                            return parser(value);
+                                        } else {
+                                            return value;
+                                        }
+                                    } else {
+                                        return rule;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            var d = css_rule('#front #md-toc', 'width', parseInt),
+                w = MarkdownReader.Dizmo.getWidth(),
                 h = MarkdownReader.Dizmo.getHeight();
 
-            MarkdownReader.Dizmo.setSize(w - 254, h);
+            MarkdownReader.Dizmo.setSize(w - d, h);
         }
 
         window.MAIN = new MarkdownReader.Main();
