63102734fb81ff3fb60f8cfc40c7f5f35aa83780
Hasan Karahan <hasan.karahan@blackhan.com>
2016-01-17 17:00:14 +0400

    DD-1186: run hooks.js only once

--- src/application.js
+++ src/application.js
@@ -4,24 +4,24 @@ var events = {};
 window.document.addEventListener('dizmoready', function () {
     jQuery.get('assets/settings.json').done(function (json) {
         if (window.MAIN === undefined) {
-            if (jQuery.isPlainObject(json)) {
-                for (var key in json) {
-                    if (json.hasOwnProperty(key)) {
-                        var value = MarkdownReader.Dizmo.load(key);
-                        if (value !== undefined) {
-                            if (value !== null) {
-                                if (typeof value === 'object') {
-                                    json[key] = jQuery.extend(true, json[key], value);
-                                } else {
-                                    json[key] = value;
-                                }
+            if (jQuery.isPlainObject(json) === false) {
+                json = JSON.parse(json);
+            }
+
+            for (var key in json) {
+                if (json.hasOwnProperty(key)) {
+                    var value = MarkdownReader.Dizmo.load(key);
+                    if (value !== undefined) {
+                        if (value !== null) {
+                            if (typeof value === 'object') {
+                                json[key] = jQuery.extend(true, json[key], value);
                             } else {
-                                json[key] = null;
+                                json[key] = value;
                             }
                         }
-
-                        MarkdownReader.Dizmo.save(key, json[key]);
                     }
+
+                    MarkdownReader.Dizmo.save(key, json[key]);
                 }
             }
 
